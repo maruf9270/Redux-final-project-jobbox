5c9c128c1195fb5981257f4c768168fe0e0396ab
maruf9270 <Maruf9270@gmail.com>
2023-04-30 22:50:09 +0600

    Started and completed 40 % of the worl

---
+++ jobbox-server/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const {MongoClient,ServerApiVersion} = require('mongodb')
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 2000;
+
+// using middelwares
+app.use(cors())
+app.use(express.json())
+
+
+// configureation for mongodb
+const uri ="mongodb+srv://maruf:HlySRHaaykFIugON@cluster0.c1kdkc3.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri,{
+    serverApi:{
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+// Connecting to the database
+async function connectToDatabase (){
+    try{
+      //Database name and collections names 
+      const db = client.db('job-box');
+      const candidate = db.collection('candidate');
+      const employer = db.collection('employer')
+
+// 1. posting employees data on registration
+        app.post('/register/candidate',async(req,res)=>{
+            const candidateData = req.body.data;
+            const result = await candidate.insertOne(candidateData);
+            res.send(result)
+            
+        })
+
+// 2. Posting employer data on database
+        app.post('/register/employer',async(req,res)=>{
+            const data = req.body.data;
+            const result = await employer.insertOne(data);
+            return result;
+
+        })
+        
+    }
+   finally{
+
+   }
+}
+
+connectToDatabase().catch((error)=>{
+    console.log(error);
+
+})
+
+
+// Api for testing the server
+app.get('/test',(req,res)=>{
+    console.log("got a request");
+    res.send('server is working properly')
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Your server is runninng on port${PORT}`);
+})
